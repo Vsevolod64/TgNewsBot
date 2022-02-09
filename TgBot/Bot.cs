b6e689d7b6a140944905465b3b9e7deffaf4af80
Vsevolod <seva0994@gmail.com>
2022-02-09 21:49:16 +0300

    add bot telegram

--- TgBot/Bot.cs
+++ TgBot/Bot.cs
@@ -8,62 +8,32 @@ namespace ZpNewsBot
 {
     internal class Bot
     {
-        public async void init()
+        TelegramBotClient botClient;
+        CancellationTokenSource cancellationToken;
+        public Bot()
         {
-            
+            botClient = new TelegramBotClient("5282483226:AAE_uS-e-qH21RroO3cFRH0nILggVkS2lb0");
+            cancellationToken = new CancellationTokenSource();
         }
-        public async void SendNews(string news, string image, string title, bool init)
+
+        async void sendMsg(string photourl, string title, string telegraphLink)
         {
-            if (init)
-            {
-                var botClient = new TelegramBotClient("5282483226:AAE_uS-e-qH21RroO3cFRH0nILggVkS2lb0");
+            Message message = await botClient.SendPhotoAsync(
+            chatId: -1001549217373,
+            photo: $"{photourl}",
+            caption: $"<a href=\"{telegraphLink}\">{title}</a>",
+            parseMode: ParseMode.Html,
+            cancellationToken: cancellationToken.Token);
+        }
 
-                using var cts = new CancellationTokenSource();
-
-                // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-                var receiverOptions = new ReceiverOptions
-                {
-                    AllowedUpdates = { } // receive all update types
-                };
-                botClient.StartReceiving(
-                    HandleUpdateAsync,
-                    HandleErrorAsync,
-                    receiverOptions,
-                    cancellationToken: cts.Token);
-
-                var me = await botClient.GetMeAsync();
-
-                Console.WriteLine($"Start listening for @{me.Username}");
-                Console.ReadLine();
-
-                // Send cancellation request to stop bot
-                cts.Cancel();
-            }
-           
-
-            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-            {
-                Message message = await botClient.SendPhotoAsync(
-                    chatId: -1001549217373,
-                    photo: image,
-                    caption: "<a href=\""+news+"\">"+title+"</a>",
-                    parseMode: ParseMode.Html,
-                    cancellationToken: cancellationToken);
-             }
-
-            Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-            {
-                var ErrorMessage = exception switch
-                {
-                    ApiRequestException apiRequestException
-                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                    _ => exception.ToString()
-                };
-
-                Console.WriteLine(ErrorMessage);
-                return Task.CompletedTask;
-            }
-        
+        async void sendMsg(string title, string telegraphLink)
+        {
+            Message message = await botClient.SendPhotoAsync(
+            chatId: -1001549217373,
+            photo: null,
+            caption: $"<a href=\"{telegraphLink}\">{title}</a>",
+            parseMode: ParseMode.Html,
+            cancellationToken: cancellationToken.Token);
+        }
     }
-}
 }
